47409b4cea414bafeb7d07f4ff5434167d8ec884
QiFuChina <517469138@qq.com>
2018-04-07 21:42:54 +0100

    Qeestion 7

--- 07maj.rkt
+++ 07maj.rkt
@@ -1,21 +1,31 @@
 #lang racket
 (define (maj list1 list2 list3)
+    ;Checking three lists if one list is null then result will be '()
 
-    (cond [(null? list1) '()] 
-          [(null? list2) '()]
-          [(null? list3) '()]
-          ;Checking three lists if one list is null then result will be '()
-          [(cond
-             [(= (car list1) (car list2))
-              (cons
-              (car list1) (maj (cdr list1) (cdr list2) (cdr list3)))])]          
-          [(cond
-             [(= (car list1) (car list3))
-              (cons
-              (car list1) (maj (cdr list1) (cdr list2) (cdr list3)))])]
-          [(cond
-             [(= (car list2) (car list3))
-              (cons
-              (car list2) (maj (cdr list1) (cdr list2) (cdr list3)))])]
-          ;Three cond statment to execute the result
+    (if (null? list1)
+        '()
+        (if (null? list2)
+            '()
+            (if (null? list3)
+                '()
+
+                ;One cond statment compares the first element of each list and recursive
+                (cond
+                  ((=(car list1) (car list2))
+                   (cons
+                    (car list1) (maj (cdr list1) (cdr list2) (cdr list3))))
+                  ((=(car list1) (car list3))
+                   (cons
+                    (car list1) (maj (cdr list1) (cdr list2) (cdr list3))))
+                  ((=(car list2) (car list3))
+                   (cons
+                    (car list2) (maj (cdr list1) (cdr list1) (cdr list2)))))
+
+
+
+                ))
+     ;cond [(null? list1) '()] 
+     ;     [(null? list2) '()]
+     ;     [(null? list3) '()]
+   
          ))
