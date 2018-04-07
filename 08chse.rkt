b865491f501a46ed3eeabb5ca0957bd67fd31a59
QiFuChina <517469138@qq.com>
2018-04-07 22:17:28 +0100

    Question 8

--- 08chse.rkt
+++ 08chse.rkt
@@ -1,15 +1,19 @@
 #lang racket
+;Question:Write a function chse in Racket that takes three lists x, y and z of equal length and containing only 0’s and 1’s. It should return a list containing the elements of y in the positions where x is 1 and the elements of z otherwise.
 (define (chse list1 list2 list3)
 
- 
+  ;Checking three lists if one list is null then result will be '()  
   (if(null? list1)
      '()
      (if(null? list2)
         '()
         (if (null? list3)
             '()
-  ;Checking three lists if one list is null then result will be '()         
+        ;One cond statment compares the first element of each list and recursive
+        ;The value of result will depend the first element of list1 and list3 
         (cond
-          [((= (car list1) 1) (cons (car list2) (chse (cdr list1) (cdr list2) (cdr list3))))]
-          [else ((cons (car list3) (chse (cdr list1) (cdr list2) (cdr list3))))]  
+          ((= (car list1) 1);If is lis1, return the list2 element each time
+           (cons (car list2) (chse (cdr list1) (cdr list2) (cdr list3))))
+          (else;If not, return the list3 element each time
+           (cons (car list3) (chse (cdr list1) (cdr list2) (cdr list3))))
           )))))
